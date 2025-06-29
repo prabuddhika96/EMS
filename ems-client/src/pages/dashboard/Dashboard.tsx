2ede7c5ef8f10366f5bbcb83c39c2a3ce2d6e5db
Prabuddhika_tecciance <prabuddhika.mayurapaksha@tecciance.com>
2025-06-29 12:38:18 +0530

    get tokken from cookie

--- ems-client/src/pages/dashboard/Dashboard.tsx
+++ ems-client/src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { Event } from "../../interface/Event";
+import { eventService } from "../../service/eventService";
 
 function Dashboard() {
   const [page, setPage] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
   const [eventList, setEventList] = useState<Event[]>([]);
+
+  const fetchEevents = async () => {
+    try {
+      const apiResponse: any = await eventService.getAllUpcomingEvents(
+        page - 1,
+        pageSize
+      );
+
+      if (apiResponse instanceof Error) {
+        console.error("Failed to retrieve data:", apiResponse.message);
+      } else {
+        console.log(apiResponse?.data);
+        if (apiResponse?.data?.code == 3004) {
+          console.log(apiResponse?.data?.data?.content);
+        }
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchEevents();
+  }, []);
   return <div>Dashboard</div>;
 }
 
---
+++ ems-client/src/service/eventService.ts
@@ -0,0 +1,30 @@
+import axiosInstance from "../interceptor/axios-interceptor";
+import type { ApiResponse } from "../interface/response";
+
+
+const servicePath = '/api/v1/events';
+
+
+const getAllUpcomingEvents = async (page: number, pageSize: number): Promise<ApiResponse> => {
+    try {
+        let url = servicePath + '/filter'
+
+        if (page && pageSize) {
+            url += `?page=${page}size=${pageSize}`
+        }
+        const response = await axiosInstance.get(url);
+        return {
+            message: "",
+            data: response.data
+        };
+    } catch (err: any) {
+        return {
+            data: null,
+            message: err.response?.data?.message || err.message || "Fetch failed",
+        };
+    }
+}
+
+export const eventService = {
+    getAllUpcomingEvents
+}
