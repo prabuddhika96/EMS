9f05243828a68c36134d9bb7ea80b1ba5c120733
Prabuddhika_tecciance <prabuddhika.mayurapaksha@tecciance.com>
2025-06-29 22:38:54 +0530

    add lazy loading

---
+++ ems-client/src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Loader() {
+  return (
+    <div className="loading-overlay">
+      <p className="loading-text">
+        <span className="loading-highlight">Event Management </span>System
+      </p>
+    </div>
+  );
+}
+
+export default Loader;
--- ems-client/src/routes/AppRoute.tsx
+++ ems-client/src/routes/AppRoute.tsx
@@ -1,55 +1,70 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
-import { RouteName } from "../constants/routeNames";
 import AuthLayout from "../layouts/AuthLayout/AuthLayout";
-import Login from "../pages/login/Login";
-import Dashboard from "../pages/dashboard/Dashboard";
-import EventDetail from "../pages/EventDetail/EventDetail";
-import Profile from "../pages/profile/Profile";
-import CreateEvent from "../pages/createEvent/CreateEvent";
-import PrivateEvents from "../pages/privateEvents/PrivateEvents";
+import { RouteName } from "../constants/routeNames";
+import Loader from "../components/Loader/Loader";
+
+const Login = lazy(() => import("../pages/login/Login"));
+const Dashboard = lazy(() => import("../pages/dashboard/Dashboard"));
+const EventDetail = lazy(() => import("../pages/EventDetail/EventDetail"));
+const Profile = lazy(() => import("../pages/profile/Profile"));
+const CreateEvent = lazy(() => import("../pages/createEvent/CreateEvent"));
+const PrivateEvents = lazy(
+  () => import("../pages/privateEvents/PrivateEvents")
+);
+
+const LoadingFallback = () => <Loader />;
 
 function AppRoute() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path={RouteName.Home}
-          element={<AuthLayout children={<Login />} />}
-        />
+      <Suspense fallback={<LoadingFallback />}>
+        <Routes>
+          <Route
+            path={RouteName.Home}
+            element={<AuthLayout children={<Login />} />}
+          />
 
-        <Route
-          path={RouteName.Dashboard}
-          element={<MainLayout children={<Dashboard />} title={"Dashboard"} />}
-        />
+          <Route
+            path={RouteName.Dashboard}
+            element={
+              <MainLayout children={<Dashboard />} title={"Dashboard"} />
+            }
+          />
 
-        <Route
-          path={RouteName.EventDetails}
-          element={
-            <MainLayout children={<EventDetail />} title={"Event Details"} />
-          }
-        />
+          <Route
+            path={RouteName.EventDetails}
+            element={
+              <MainLayout children={<EventDetail />} title={"Event Details"} />
+            }
+          />
 
-        <Route
-          path={RouteName.Profile}
-          element={<MainLayout children={<Profile />} title={"User Profile"} />}
-        />
+          <Route
+            path={RouteName.Profile}
+            element={
+              <MainLayout children={<Profile />} title={"User Profile"} />
+            }
+          />
 
-        <Route
-          path={RouteName.CreateEvent}
-          element={
-            <MainLayout children={<CreateEvent />} title={"Create Event"} />
-          }
-        />
+          <Route
+            path={RouteName.CreateEvent}
+            element={
+              <MainLayout children={<CreateEvent />} title={"Create Event"} />
+            }
+          />
 
-        <Route
-          path={RouteName.PrivateEvent}
-          element={
-            <MainLayout children={<PrivateEvents />} title={"Private Events"} />
-          }
-        />
-      </Routes>
+          <Route
+            path={RouteName.PrivateEvent}
+            element={
+              <MainLayout
+                children={<PrivateEvents />}
+                title={"Private Events"}
+              />
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
